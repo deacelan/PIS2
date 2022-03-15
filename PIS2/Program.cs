7b6a8416af92c4a5a286427563660d34839a2231
Luka Baranovic <73253333+LukaBaranovic@users.noreply.github.com>
2022-03-15 16:48:19 +0100

    update

--- PIS2/Program.cs
+++ PIS2/Program.cs
@@ -6,7 +6,7 @@ namespace PIS2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Helloooo World!");
             Console.WriteLine("Hi");
         }
     }
