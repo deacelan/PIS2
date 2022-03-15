51e3f6244dba1c8a2587c334dadc3d2e56850e1f
deacelan <deacelan23@gmail.com>
2022-03-15 16:43:25 +0100

    prvi commit

---
+++ PIS2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
